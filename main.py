94dad65d953dadfa0a2c170eaefb7427bf9900ab
Rajneesh Kumar <97952294+AryaRaj76438@users.noreply.github.com>
2022-03-22 08:31:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = shift_amount + position
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"The {cipher_direction}d text is {end_text}")
+
+print(art.logo)
+
+check_for_continue = True
+while check_for_continue is not False:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    to_restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if to_restart == "yes":
+        check_for_continue = True
+    else:
+        check_for_continue = False
+
+print("Good Bye")
+
+
